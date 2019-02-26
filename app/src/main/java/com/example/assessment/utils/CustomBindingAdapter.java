688d6bea4adca42df9cfcfd7637c35d73e538c2a
rfkamd <rafiq.ahmed.27@gmail.com>
2019-02-26 18:31:53 +0500

    added article row databinded

--- app/src/main/java/com/example/assessment/utils/CustomBindingAdapter.java
+++ app/src/main/java/com/example/assessment/utils/CustomBindingAdapter.java
@@ -1,15 +1,37 @@
 package com.example.assessment.utils;
 
 import android.databinding.BindingAdapter;
+import android.media.Image;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.assessment.network.models.MediaMetadata;
+import com.example.assessment.network.models.Medium;
+
+import java.util.List;
 
 public class CustomBindingAdapter {
 
     @BindingAdapter("app:url")
     public void setUrl(String url, ImageView imageView){
         Glide.with(imageView.getContext()).load(url).into(imageView);
+    }
+
+    @BindingAdapter("app:views")
+    public void setViews(int num, TextView textView){
+        textView.setText(String.valueOf(num));
+    }
+
+    @BindingAdapter("app:images")
+    public void setImages(List<Medium> media, ImageView imageView){
+        List<MediaMetadata> images = media.get(0).mediaMetadata;
+
+        for (MediaMetadata image : images) {
+            if(image.format.equalsIgnoreCase("Standard Thumbnail")){
+                setUrl(image.url, imageView);
+            }
+        }
     }
 
 }
